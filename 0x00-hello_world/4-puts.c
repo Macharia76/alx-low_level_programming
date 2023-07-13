dd7fcb897c5075e69a2f7f7aa39fde978dc4f354
Macharia76 <ngugisamuel218@gmail.com>
2023-07-13 04:29:12 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A program that prints a line with the puts function
+ *puts - writes the given string to the standard output stream stdout
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+	
