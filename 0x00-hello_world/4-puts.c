55bbd0d1562b6a212593f24145b950b7868f4400
Macharia76 <ngugisamuel218@gmail.com>
2023-07-13 05:01:44 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - A program that prints a line with the puts function
- *puts - writes the given string to the standard output stream stdout
+ * main - A program that prints a line puts function
+ * puts - writes the given string to the standard output stream stdout
+ * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-	
