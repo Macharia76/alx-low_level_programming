f422a8ced2da2fb741739979c16f218d7bcc1d10
Macharia76 <ngugisamuel218@gmail.com>
2023-08-16 11:14:49 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -14,14 +14,14 @@
 typedef struct op
 {
 	char *op;
-	int (*m)(int j, int k);
+	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int j, int k);
-int op_sub(int j, int k);
-int op_mul(int j, int k);
-int op_div(int j, int k);
-int op_mod(int j, int k);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif
