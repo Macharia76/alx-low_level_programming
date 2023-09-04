315c66ea8f5d27c35ff30c065dfa96b5212d5440
Macharia76 <ngugisamuel218@gmail.com>
2023-09-04 03:01:20 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,28 +10,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	size_t len = 0;
-	ssize_t check = 0;
+	int fd, q, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	/*Find length of content*/
-	while (text_content[len] && text_content)
-		len++;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
 	if (text_content != NULL)
-		check = write(fd, text_content, len);
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (check == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	q = write(fd, text_content, len);
+
+	if (fd == -1 || q == -1)
 		return (-1);
 
 	close(fd);
+
 	return (1);
 }
 
